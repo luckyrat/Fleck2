8a22a58e0eecd19c5f783109d272604440702652
username1565 <39200328+username1565@users.noreply.github.com>
2019-10-23 18:35:11 +0300

    Fix 2 files
    Smal fixes in "/src/FleckLog.cs", and "/src/WebSocketServer.cs"

--- src/WebSocketServer.cs
+++ src/WebSocketServer.cs
@@ -23,11 +23,6 @@ namespace Fleck2
         {
         }
 
-        public WebSocketServer(int port, string location)
-            : this(8181, location, false)
-        {
-        }
-
         public WebSocketServer(int port, string location, bool bindOnlyToLoopback)
         {
             var uri = new Uri(location);
@@ -77,12 +72,12 @@ the loopback interface only.
             var ipLocal = new IPEndPoint(ipAddress, Port);
             ListenerSocket.Bind(ipLocal);
             ListenerSocket.Listen(100);
-            FleckLog.Info("Server started at " + Location,null);
+            FleckLog.Info("Server started at " + Location);
             if (_scheme == "wss")
             {
                 if (Certificate == null)
                 {
-                    FleckLog.Error("Scheme cannot be 'wss' without a Certificate",null);
+                    FleckLog.Error("Scheme cannot be 'wss' without a Certificate");
                     return;
                 }
             }
